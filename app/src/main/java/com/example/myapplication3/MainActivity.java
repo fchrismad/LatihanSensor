4b32aa99f13991a73f3405792e9a0f77fd22efc1
fachri <you@example.com>
2023-03-14 13:56:09 +0700

    TugasSensor

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -19,9 +19,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager mSensorManager;
     private Sensor mSensorLight;
     private Sensor mSensorProximity;
+    private Sensor mSensorTemperature;
+    private Sensor mSensorPressure;
+    private Sensor mSensorHumidity;
 
     private TextView mTextSensorLight;
     private TextView mTextSensorProximity;
+    private TextView mTextSensorTemperature;
+    private TextView mTextSensorPressure;
+    private TextView mTextSensorHumidity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +37,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         mTextSensorLight = findViewById(R.id.label_light);
         mTextSensorProximity = findViewById(R.id.label_proximity);
+        mTextSensorTemperature = findViewById(R.id.label_temperature);
+        mTextSensorPressure = findViewById(R.id.label_pressure);
+        mTextSensorHumidity = findViewById(R.id.label_humidity);
 
         mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         mSensorProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mSensorTemperature = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        mSensorPressure = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        mSensorHumidity = mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
 
         String sensor_error = "No sensor";
 
@@ -43,7 +55,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (mSensorProximity == null){
             mTextSensorProximity.setText(sensor_error);
         }
-
+        if (mSensorTemperature == null){
+            mTextSensorTemperature.setText(sensor_error);
+        }
+        if (mSensorPressure == null){
+            mTextSensorPressure.setText(sensor_error);
+        }
+        if (mSensorHumidity == null){
+            mTextSensorHumidity.setText(sensor_error);
+        }
     }
 
     @Override
@@ -55,6 +75,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
         if (mSensorLight != null){
             mSensorManager.registerListener(this, mSensorLight,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mSensorTemperature != null){
+            mSensorManager.registerListener(this, mSensorTemperature,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mSensorPressure != null){
+            mSensorManager.registerListener(this, mSensorPressure,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mSensorHumidity != null){
+            mSensorManager.registerListener(this, mSensorHumidity,
                     SensorManager.SENSOR_DELAY_NORMAL);
         }
     }
@@ -79,6 +111,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             case Sensor.TYPE_PROXIMITY:
                 mTextSensorProximity.setText(
                         String.format("Proximity Sensor : %1$.2f", currentValue));
+                break;
+            case Sensor.TYPE_AMBIENT_TEMPERATURE:
+                mTextSensorTemperature.setText(
+                        String.format("Ambient Temperature Sensor : %1$.2f", currentValue));
+                break;
+            case Sensor.TYPE_PRESSURE:
+                mTextSensorPressure.setText(
+                        String.format("Pressure Sensor : %1$.2f", currentValue));
+                break;
+            case Sensor.TYPE_RELATIVE_HUMIDITY:
+                mTextSensorHumidity.setText(
+                        String.format("Relative Humidity Sensor : %1$.2f", currentValue));
                 break;
             default:
                 //
